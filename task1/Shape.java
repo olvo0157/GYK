fb7dbaa42bb193e2bc5689b96387964ff9151972
olvo0157 <117648129+olvo0157@users.noreply.github.com>
2022-11-30 08:29:41 +0200

    Add files via upload

---
+++ task1/Shape.java
@@ -0,0 +1,7 @@
+package com.gyk.java.oop.basics.task1;
+
+public abstract class Shape {
+    abstract void calculateArea();
+    abstract void calculatePerimeter();
+}
+
---
+++ task2/Bike.java
@@ -0,0 +1,9 @@
+package com.gyk.java.oop.basics.task2;
+
+public enum Bike {MOUNTAIN,ROAD,NONE
+
+}
+
+/*
+enum Bike with values mountain and road
+ */
