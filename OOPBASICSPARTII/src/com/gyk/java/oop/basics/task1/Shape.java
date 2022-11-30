80a93ed329c679d18a6cc613ec2bda91872e2832
olvo0157 <117648129+olvo0157@users.noreply.github.com>
2022-11-30 08:32:43 +0200

    Delete OOPBASICSPARTII directory

--- OOPBASICSPARTII/src/com/gyk/java/oop/basics/task1/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.gyk.java.oop.basics.task1;
-
-public abstract class Shape {
-    abstract void calculateArea();
-    abstract void calculatePerimeter();
-}
-
--- OOPBASICSPARTII/src/com/gyk/java/oop/basics/task2/Bike.java
+++
@@ -1,9 +0,0 @@
-package com.gyk.java.oop.basics.task2;
-
-public enum Bike {MOUNTAIN,ROAD,NONE
-
-}
-
-/*
-enum Bike with values mountain and road
- */
